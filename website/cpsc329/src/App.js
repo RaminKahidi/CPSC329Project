a5ba45252b206f15ec46d4e90fe7a26da30f3929
Zhai790 <michael.cagm@gmail.com>
2022-12-02 17:02:01 -0800

    created/styled footer

--- website/cpsc329/src/App.js
+++ website/cpsc329/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
-import Navbar from './components/Navbar.js'
+import Navbar from './components/Navbar'
 import './components/Navbar.css'
+import Footer from './components/Footer'
+import './components/Footer.css'
 import './App.css'
 
 function App() {
   return (
     <>
         <Navbar />
+        <Footer />
     </>
   )
 }
